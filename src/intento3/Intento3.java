776cd79eead7bfe4e547e7371052d7fabc7283f9
cyber-JuanDa <juanda.o.ochoa@gmail.com>
2025-05-09 18:45:51 -0500

    Update Intento3.java

--- src/intento3/Intento3.java
+++ src/intento3/Intento3.java
@@ -7,7 +7,7 @@ public class Intento3 {
     
     public static void main(String[] args) {
         
-        String name; 
+        String name2; 
         String edad; 
         int e;
         String x;
